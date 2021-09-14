a7d0e7f43769a478f2128aee5bf89a9ca875707d
Tony Ketcham <ketcham@pnw.edu>
2021-09-13 20:41:01 -0500

    add(layout): footer component

---
+++ src/lib/components/general/Footer.svelte
@@ -0,0 +1,35 @@
+<script>
+	import Logo from '$lib/assets/Reset.svelte';
+	import SocialLinks from '../SocialLinks.svelte';
+</script>
+
+<hr
+	class="h-2 w-full bg-gradient-to-r from-purple-400 to-pink-600 border-0 border-transparent drop-shadow-xl"
+/>
+<footer class="w-full p-8">
+	<nav>
+		<ul class="flex justify-between">
+			<li class="w-72">
+				<h1 class="w-24 inline-block">
+					<a sveltekit:prefetch href="/">
+						<Logo />
+					</a>
+				</h1>
+				<small class="opacity-40">music for the rest of us</small>
+			</li>
+			<li class="w-max-w">
+				<ul class="flex justify-center space-x-4 font-mono">
+					<li>
+						<a sveltekit:prefetch href="/events"> events </a>
+					</li>
+					<li>
+						<a sveltekit:prefetch href="/about"> about</a>
+					</li>
+				</ul>
+			</li>
+			<li class="flex justify-end w-72 place-self-center">
+				<SocialLinks />
+			</li>
+		</ul>
+	</nav>
+</footer>
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,20 +1,16 @@
 <script>
 	// Component imports
 	import Header from '$lib/components/general/Header.svelte';
+	import Footer from '$lib/components/general/Footer.svelte';
 
 	// Tailwind and that jazz
 	import '../app.postcss';
 </script>
 
-<div class="bg-black text-white min-h-screen font-mono">
+<div class="bg-black flex flex-col text-white min-h-screen font-mono">
 	<Header />
-	<slot />
+	<main class="flex-1">
+		<slot />
+	</main>
+	<Footer />
 </div>
-<section class="bg-black min-h-screen flex align-middle">
-	<!-- <iframe
-		title="Join the Reset Discord"
-		src="https://e.widgetbot.io/channels/614710925714391040/614847723920818176"
-		loading="lazy"
-		class="w-[80vw] md:w-[90vw] h-[80vh] m-auto transition-all ease-in-out duration-300 ring-2 ring-offset-0 hover:ring-offset-4 ring-offset-black ring-transparent hover:ring-purple-400 hover:ring-opacity-40 rounded-md bg-[#36393F]"
-	/> -->
-</section>
