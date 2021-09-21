a870e92426a635d3f0e5d80a23a7a7d2778d6b44
Tony Ketcham <ketcham@pnw.edu>
2021-09-21 18:42:14 -0500

    add(core): custom Tailwind color palette

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,12 +1,23 @@
+const colors = require('tailwindcss/colors');
+
 const config = {
-	mode: "jit",
-	purge: [
-		"./src/**/*.{html,js,svelte,ts}",
-	],
+	mode: 'jit',
+	purge: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
-		extend: {},
+		colors: {
+			transparent: 'transparent',
+			current: 'currentColor',
+			black: colors.black,
+			white: colors.white,
+			gray: colors.gray,
+			indigo: colors.indigo,
+			purple: colors.purple,
+			red: colors.rose,
+			pink: colors.pink,
+			yellow: colors.amber
+		}
 	},
-	plugins: [],
+	plugins: []
 };
 
 module.exports = config;
