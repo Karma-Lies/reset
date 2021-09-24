d799dd852116690606c7affec031c56a0e25c648
Tony Ketcham <ketcham@pnw.edu>
2021-09-23 20:05:20 -0500

    feat(core): contact page

---
+++ src/routes/contact.svelte
@@ -0,0 +1,16 @@
+<script>
+	// Component imports
+	import SEO from '$lib/components/01. atoms/SEO.svelte';
+
+	const email = 'resetpresents@gmail.com';
+</script>
+
+<SEO title="Contact" url="contact" />
+<section id="contact" class="m-12 p-4">
+	<h1 class="text-4xl">Contact</h1>
+	<main>
+		<p>
+			sauce it on over to <a href="mailto:{email}" class="standard-link text-indigo-400">{email}</a>
+		</p>
+	</main>
+</section>
