bb063941568d6dc1620abcea02e1892d4b5e91da
Tony Ketcham <tonyketcham@gmail.com>
2021-09-25 13:40:34 -0500

    styling(core): Tailwind config extension

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,7 @@ const colors = require('tailwindcss/colors');
 
 const config = {
 	mode: 'jit',
+	darkMode: 'media',
 	purge: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
 		colors: {
@@ -15,6 +16,12 @@ const config = {
 			red: colors.rose,
 			pink: colors.pink,
 			yellow: colors.amber
+		},
+		extend: {
+			dropShadow: {
+				'bonk-light': '0rem 0.25rem 0.25rem #eee',
+				'bonk-dark': '0rem 0.25rem 0.25rem #222'
+			}
 		}
 	},
 	plugins: []
