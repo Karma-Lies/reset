66616e0227aa297d37ad6407c82fb2851d7e1c55
Tony Ketcham <tonyketcham@gmail.com>
2021-09-28 22:13:57 -0500

    styling(core): extend colors and fonts

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -10,17 +10,22 @@ const config = {
 			current: 'currentColor',
 			black: colors.black,
 			white: colors.white,
-			gray: colors.gray,
+			gray: colors.warmGray,
 			indigo: colors.indigo,
 			purple: colors.purple,
 			red: colors.rose,
 			pink: colors.pink,
-			yellow: colors.amber
+			yellow: colors.amber,
+			'crisp-dark': '#161616'
 		},
 		extend: {
 			dropShadow: {
 				'bonk-light': '0rem 0.25rem 0.25rem #eee',
 				'bonk-dark': '0rem 0.25rem 0.25rem #222'
+			},
+			fontFamily: {
+				archivo: 'Archivo Black',
+				lexend: 'Lexend'
 			}
 		}
 	},
