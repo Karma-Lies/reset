fa9eb14f2ca791ff93b808b1fd9334d3ed9e961b
Tony Ketcham <tonyketcham@gmail.com>
2021-09-28 23:16:31 -0500

    fix(ArtistList): duplicate keys

--- src/lib/components/02. molecules/ArtistList.svelte
+++ src/lib/components/02. molecules/ArtistList.svelte
@@ -2,16 +2,16 @@
 	import SocialLinks from './SocialLinks.svelte';
 
 	export let artists = [
-		{ name: 'JKuch', slug: 'cam-stacey', spotify: 'https://google.com' },
-		{ name: 'Xavi', slug: 'cam-stacey', spotify: 'https://google.com' },
-		{ name: 'ell!psis', slug: 'cam-stacey', spotify: 'https://google.com' },
-		{ name: 'Sweater Disco', slug: 'cam-stacey', spotify: 'https://google.com' },
-		{ name: 'Waxcraft', slug: 'cam-stacey', spotify: 'https://google.com' }
+		{ name: 'JKuch', spotify: 'https://google.com' },
+		{ name: 'Xavi', spotify: 'https://google.com' },
+		{ name: 'ell!psis', spotify: 'https://google.com' },
+		{ name: 'Sweater Disco', spotify: 'https://google.com' },
+		{ name: 'Waxcraft', spotify: 'https://google.com' }
 	];
 </script>
 
 <ul name="artists" class="space-y-2 w-prose">
-	{#each artists as artist (artist.slug)}
+	{#each artists as artist (artist.name)}
 		<li class="flex space-x-4">
 			<a
 				class="test font-medium bg-clip-text text-transparent bg-gradient-to-br from-purple-100 via-purple-200 to-purple-500"
