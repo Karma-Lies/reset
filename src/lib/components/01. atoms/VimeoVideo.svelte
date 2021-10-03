d02bb714ecb76d126b64cd1312e70a7bc590ecdf
Tony Ketcham <tonyketcham@gmail.com>
2021-10-03 08:10:46 -0500

    feat(atoms): vimeo video embed component

---
+++ src/lib/components/01. atoms/VimeoVideo.svelte
@@ -0,0 +1,23 @@
+<script>
+	// Props
+	export let src = 'https://vimeo.com/556675018';
+	export let color = '#ffffff';
+
+	// Library imports
+	import { onMount } from 'svelte';
+	import Player from '@vimeo/player';
+
+	let video;
+	const tokenizedURL = src.split('/');
+
+	onMount(() => {
+		// https://github.com/vimeo/player.js#embed-options
+		const player = new Player(video, {
+			id: tokenizedURL.pop(),
+			color: color,
+			responsive: true
+		});
+	});
+</script>
+
+<div bind:this={video} class="w-full h-full" />
