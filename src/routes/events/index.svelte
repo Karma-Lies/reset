8da29f17ba158e59f4a6c908efb46f8d20625c8b
Tony Ketcham <tonyketcham@gmail.com>
2021-10-03 10:36:24 -0500

    styling(routes): brutalist madness

--- src/routes/events/index.svelte
+++ src/routes/events/index.svelte
@@ -27,13 +27,79 @@
 	export let stats;
 </script>
 
-<section id="upcoming-events" class="m-6">
-	<h2 class="text-2xl">Events by year</h2>
+<!-- 	class="md:justify-center pt-12 md:pt-0 h-full min-h-[40rem] space-y-3" -->
+<section id="upcoming-events">
+	<div
+		id="dope-gradient"
+		class="before:right-0 before:left-0 before:top-0 before:h-full relative before:absolute before:from-crisp-dark before:via-transparent before:bg-gradient-to-b z-0"
+	>
+		<img
+			src="/static/photos/Fraxiom.jpeg"
+			alt="Fraxiom in 2020"
+			width="1095"
+			height="1095"
+			lazyload
+			class="w-5/6 lg:w-1/2 h-full min-h-[16rem] object-cover object-bottom pr-2"
+		/>
+		<p id="cta" class="z-10">pick a year</p>
+	</div>
+
+	<header>
+		<h2>Events<br /> by<br /> year</h2>
+	</header>
+
 	<ul>
 		{#each stats as metadata}
-			<li>
-				<a href="/events/{metadata.year}"> <h3 class="text-xl">{metadata.year}</h3></a>
+			<li class="group">
+				<a href="/events/{metadata.year}">
+					<h3>{metadata.year}</h3>
+					<span
+						class="opacity-0 group-hover:opacity-100 text-sm absolute tracking-widest right-4 bottom-4 md:bottom-8 px-1 rounded-sm shadow-lg bg-indigo-700"
+						>explore {metadata.eventCount} {metadata.eventCount.length > 1 ? 'shows' : 'show'}</span
+					></a
+				>
 			</li>
 		{/each}
 	</ul>
+	<div id="radials" />
 </section>
+
+<style lang="postcss">
+	section {
+		@apply grid grid-cols-2;
+	}
+	#dope-gradient {
+		@apply flex justify-between col-start-1 col-end-3 w-full max-h-80 md:max-h-full pointer-events-none;
+		@apply bg-gradient-to-t from-indigo-700;
+	}
+	#cta {
+		@apply text-lg tracking-wider my-2 md:my-4 border-l-2 text-gray-300;
+		writing-mode: vertical-rl;
+	}
+	#radials {
+		@apply col-start-3 col-end-7 w-full h-full pointer-events-none;
+		background: repeating-radial-gradient(
+			circle,
+			rgb(67, 56, 202),
+			rgb(67, 56, 202) 4px,
+			transparent 4px,
+			transparent 32px
+		);
+	}
+	header {
+		@apply col-start-1 col-end-3;
+	}
+	h2 {
+		@apply text-7xl md:text-9xl uppercase relative line-through font-heading my-2;
+		line-height: 0.8;
+	}
+	ul {
+		@apply col-start-3 col-end-7 row-start-1 row-span-full place-self-start;
+		@apply text-7xl md:text-9xl font-heading  rounded-sm;
+	}
+	li {
+		@apply relative mx-auto max-w-max;
+		@apply hover:text-yellow-400 transition-colors bg-transparent hover:bg-crisp-dark rounded-sm px-2;
+		@apply even:text-indigo-600 hover:even:text-yellow-400;
+	}
+</style>
