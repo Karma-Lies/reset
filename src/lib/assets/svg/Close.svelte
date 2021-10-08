c40e8f21881d1751e9db90cee7811dd440f981cb
Tony Ketcham <tonyketcham@gmail.com>
2021-10-08 14:25:43 -0500

    feat(molecules): modal component basics

---
+++ src/lib/assets/svg/Close.svelte
@@ -0,0 +1,12 @@
+<svg
+	xmlns="http://www.w3.org/2000/svg"
+	viewBox="0 0 24 24"
+	fill="none"
+	stroke="currentColor"
+	stroke-width="2"
+	stroke-linecap="round"
+	stroke-linejoin="round"
+	class="w-full"
+>
+	<path d="M20 20L4 4m16 0L4 20" />
+</svg>
---
+++ src/lib/components/02. molecules/Modal.svelte
@@ -0,0 +1,32 @@
+<script>
+	import Close from '$lib/assets/svg/Close.svelte';
+	import { createEventDispatcher } from 'svelte';
+
+	const dispatch = createEventDispatcher();
+	const closeModal = () => dispatch('close');
+</script>
+
+<div
+	class="fixed inset-0 w-full flex flex-col h-screen overscroll-none overflow-hidden z-40 p-4 group"
+>
+	<div
+		on:click={closeModal}
+		class="absolute inset-0 bg-crisp-dark bg-opacity-30 h-full z-40 duration-500 backdrop-hue-rotate-0 group-hover:-backdrop-hue-rotate-90 cursor-pointer transition-all"
+	/>
+	<div
+		class="absolute m-auto inset-4 max-w-md max-h-[32rem] overflow-scroll bg-gray-800 border-2 border-gray-400 border-opacity-20 rounded z-50 drop-shadow-2xl"
+		style="height: fit-content;"
+	>
+		<slot
+			><div class="w-full h-full flex">
+				<p class="m-auto font-semibold">Place content here</p>
+			</div></slot
+		>
+		<div
+			class="absolute right-2 top-2 w-4 h-4 text-gray-400 hover:text-gray-200 select-none transition-colors cursor-pointer"
+			on:click={closeModal}
+		>
+			<Close />
+		</div>
+	</div>
+</div>
