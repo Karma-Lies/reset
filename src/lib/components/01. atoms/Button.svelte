39ba41f5336c1f12feae257a0cf3e08a85ba9282
Tony Ketcham <tonyketcham@gmail.com>
2021-10-09 17:41:20 -0500

    feat(atoms): stateful button component

---
+++ src/lib/components/01. atoms/Button.svelte
@@ -0,0 +1,35 @@
+<script>
+	export let state = 'default';
+	export let potentialStates = {
+		error: '🚭 uh oh!',
+		success: '🧑‍🌾 Success!',
+		default: '🧙‍♀️ Click it'
+	};
+	export let type = undefined;
+	export let size = 'md';
+
+	// Reactive button text based on current button state
+	$: buttonInnerText = potentialStates[state];
+</script>
+
+<button
+	{type}
+	on:click
+	class="float-right w-full px-6 py-2 text-black transition-all bg-gray-100 rounded-sm max-w-max hover:bg-gray-700 hover:text-white drop-shadow-sm disabled:hover:bg-indigo-500 disabled:hover:text-black disabled:cursor-not-allowed"
+	class:large={size === 'lg'}
+	class:small={size === 'sm'}
+	class:bg-indigo-500={state === 'success'}
+	class:bg-red-100={state === 'error'}
+	disabled={state === 'success'}
+>
+	{buttonInnerText}
+</button>
+
+<style lang="postcss">
+	.large {
+		@apply font-semibold text-lg;
+	}
+	.small {
+		@apply font-medium text-sm;
+	}
+</style>
