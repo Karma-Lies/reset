37594dd7e8c556236c4b1db5123011474336f443
Tony Ketcham <tonyketcham@gmail.com>
2021-10-12 05:30:12 -0500

    add(atoms): popup label for input fields

---
+++ src/lib/components/01. atoms/PopupLabel.svelte
@@ -0,0 +1,9 @@
+<script>
+	export let target;
+</script>
+
+<label
+	for={target}
+	class="absolute inline-block top-0 bottom-0 group-focus-within:text-gray-500 text-black font-medium left-1 group-focus-within:-translate-y-7 motion-safe:transition-all"
+	><slot /></label
+>
