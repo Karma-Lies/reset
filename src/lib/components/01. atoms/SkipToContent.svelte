1ef7ac218da1c9f760dc4c9acbe187830d8d8636
Tony Ketcham <tonyketcham@gmail.com>
2021-10-12 05:33:53 -0500

    add(a11y): skip to main content

---
+++ src/lib/components/01. atoms/SkipToContent.svelte
@@ -0,0 +1,5 @@
+<a
+	class="sr-only focus:not-sr-only focus:fixed focus:top-2 focus:left-2 focus:p-2 focus:bg-gray-900 focus:m-1"
+	tabindex="0"
+	href="#main-content">Skip to main content</a
+>
--- src/lib/components/03. modules/Header.svelte
+++ src/lib/components/03. modules/Header.svelte
@@ -1,22 +1,29 @@
 <script>
+	import SkipToContent from './../01. atoms/SkipToContent.svelte';
 	// Component imports
 	import Logo from '$svg/Reset.svelte';
 </script>
 
 <header>
+	<SkipToContent />
 	<nav
 		class="mx-auto text-lg text-black pointer-events-auto max-w-max dark:text-gray-100 drop-shadow-bonk-light dark:drop-shadow-bonk-dark"
+		role="navigation"
+		label="Primary"
 	>
 		<ul class="flex space-x-4 text-sm justify-evenly place-items-center md:text-base font-heading">
 			<li class="w-16 text-right">
 				<a class="text-gray-200 standard-link" sveltekit:prefetch href="/events"> events </a>
 			</li>
 			<li>
-				<h1 class="w-40 md:w-64">
-					<a sveltekit:prefetch href="/">
-						<Logo />
-					</a>
-				</h1>
+				<a
+					aria-label="Reset Presents Logo. Click to visit the home page."
+					sveltekit:prefetch
+					href="/"
+					class="inline-block w-40 md:w-64"
+				>
+					<Logo />
+				</a>
 			</li>
 			<li class="w-16 text-left">
 				<a class="text-gray-200 standard-link" sveltekit:prefetch href="/about"> about </a>
