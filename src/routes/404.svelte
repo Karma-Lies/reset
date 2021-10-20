c6f236d8139c21af80b95da8f87f3aecde4cd566
Tony Ketcham <ketcham@pnw.edu>
2021-10-19 22:00:42 -0500

    feat(core): Netlify-jazzed 404 time

---
+++ src/routes/404.svelte
@@ -0,0 +1,13 @@
+<svelte:head>
+	<title>uh oh! 404</title>
+	<meta name="robots" content="noindex" />
+</svelte:head>
+
+<section id="main-content" class="grid place-content-center h-[32rem]">
+	<h1 class="text-9xl font-heading text-gray-100 text-center">404</h1>
+	<p class="text-center border-2 p-2 border-white rounded-sm font-mono text-gray-100 text-lg">
+		1 cat 2 cat 3 cat 4
+	</p>
+	<p class="text-center text-gray-200 mt-2 text-sm">we live in a simulation</p>
+	<span class="text-gray-200">weee!</span>
+</section>
--- src/routes/__error.svelte
+++ src/routes/__error.svelte
@@ -16,7 +16,7 @@
 </svelte:head>
 
 <section id="main-content" class="grid place-content-center h-[32rem]">
-	<h1 class="text-9xl font-heading text-gray-100">{status}</h1>
+	<h1 class="text-9xl font-heading text-gray-100 text-center">{status}</h1>
 	<p class="text-center border-2 p-2 border-white rounded-sm font-mono text-gray-100 text-lg">
 		{error.message}
 	</p>
